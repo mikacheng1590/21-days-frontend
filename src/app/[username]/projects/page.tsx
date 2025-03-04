6e38900dc35b5330c6bf118b580c29a8e2b3aea9
Mika Fung <fungcheukman@hotmail.com>
2025-03-03 22:08:52 -0800

    update max width to projects table

--- src/app/[username]/projects/page.tsx
+++ src/app/[username]/projects/page.tsx
@@ -20,8 +20,8 @@ export default async function ProjectsPage({
   const isOwner = await isPageOwner(username)
   
   return (
-    <div className="min-h-screen p-4">
-      <div className="max-w-4xl mx-auto">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-2xl font-bold">This is a page</h1>
           {isOwner &&
